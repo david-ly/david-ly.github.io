87fbcc72d82c2d0717ce30a03b8bb41f52156841
David Ly <david.ly@berkeley.edu>
2024-12-22 19:53:55 -0800

    refactor(lint): modularize config options for js/ts-adjacent files

--- eslint.config.js
+++ eslint.config.js
@@ -4,42 +4,63 @@ import ts from "typescript-eslint"
 
 const {browser, node} = globals
 
+const ASTRO_IGNORE = ['.astro/*']
+const RULES_ALL = {
+  'eol-last': ['error', 'always']
+, 'linebreak-style': ['error', 'unix']
+}
+const RULES_JS = {
+  'comma-style': ['error', 'first']
+, 'comma-spacing': ['error', {before: false, after: true}]
+, 'semi': ['error', 'never']
+}
+
+const common_js = createFileTypeConfig('**/*.{js,cjs}')
+const module_js = createFileTypeConfig('**/*.{mjs,ts}')
+const tailwind = createFileTypeConfig('**/tailwind.config.cjs')
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*"]
+    files: ['**/*']
   , ignores: [
-      ".astro/*"
-    , "node_modules/*"
-    , "dist/*"
+      '.astro/*'
+    , 'node_modules/*'
+    , 'dist/*'
     ]
-  , rules: {
-      'eol-last': ['error', 'always']
-    , 'linebreak-style': ['error', 'unix']
-    }
+  , rules: RULES_ALL
   }
-, {
-    ignores: [".astro/*"]
-  }
-, {
-    files: ["**/*.{js,mjs,cjs,ts}"]
-  , languageOptions: {globals: {...browser}}
-  }
-, {
-    files: ["**/tailwind.config.cjs"]
-  , languageOptions: {globals: {...node}}
-  }
-, {
-    files: ["**/*.{mjs,ts}"]
-  , languageOptions: {sourceType: "module"}
-  }
-, {
-    rules: {
-      'comma-style': ['error', 'first']
-    , 'comma-spacing': ['error', {before: false, after: true}]
-    , 'semi': ['error', 'never']
-    }
-  }
-  , js.configs.recommended
-  , ...ts.configs.recommended
+, {...common_js}
+, {...module_js}
+, {...tailwind}
+, js.configs.recommended
+, ...ts.configs.recommended
 ]
+
+function createFileTypeConfig(files) {
+  const common = {
+    files: [`${files}`]
+  , ignores: ASTRO_IGNORE
+  , rules: RULES_JS
+  }
+  // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const {ignores, ...rest} = common
+
+  switch (files) {
+    case '**/*.{js,cjs}':
+      return {
+        ...common
+      , languageOptions: {globals: {...browser}}
+      }
+    case '**/*.{mjs,ts}':
+      return {
+        ...common
+      , languageOptions: {sourceType: 'module'}
+      }
+    case '**/tailwind.config.cjs':
+      return {
+        ...common
+      , languageOptions: {globals: {...node}}
+      }
+  }
+}
