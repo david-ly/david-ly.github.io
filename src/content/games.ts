62d08916b594b70f9a82e4971ee1fa148b2c123b
David Ly <david.ly@berkeley.edu>
2025-06-19 01:53:17 -0700

    feat(content): add initial page layout & flesh out `games.ts`
    Scaffold setup/layout for page detailing the directory/list of
    arcade games I've worked on. This commit is more focused on filling
    out the content of `games.ts` with the entirety of cabs managed,
    their details/categorization, and misc/minor descriptions.

---
+++ src/content/games.ts
@@ -0,0 +1,953 @@
+export const games = [
+  {
+    name: "Marvel Adventure Lab",
+    manufacturer: "Apple Industries / FacePlace",
+    primary: "Photobooth",
+    secondary: "Redemption"
+  },
+  {
+    name: "Disney Photobooth",
+    manufacturer: "Apple Industries / FacePlace",
+    primary: "Photobooth",
+    secondary: "Redemption"
+  },
+  {
+    name: "Photostudio Deluxe",
+    manufacturer: "Apple Industries / FacePlace",
+    primary: "Photobooth",
+    secondary: "Redemption"
+  },
+  {
+    name: "2 Minute Drill",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Air FX",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Air Slide DX",
+    manufacturer: "Komuse",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Andamiro Redeem Machine",
+    manufacturer: "Andamiro",
+    primary: "Redemption"
+  },
+  {
+    name: "Angry Birds Coin Crash",
+    manufacturer: "LAI",
+    primary: "Redemption"
+  },
+  {
+    name: "Asphalt 9 Legends (Arcade) DX",
+    manufacturer: "LAI",
+    primary: "Racing"
+  },
+  {
+    name: "Balance Checker",
+    manufacturer: "Andamiro",
+    primary: "Other"
+  },
+  {
+    name: "Baseball Pro",
+    manufacturer: "Andamiro",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Basketball Pro",
+    manufacturer: "Andamiro",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Batman",
+    manufacturer: "Raw Thrills",
+    primary: "Racing"
+  },
+  {
+    name: "Bean Bag Toss",
+    manufacturer: "ICE",
+    primary: "Redemption"
+  },
+  {
+    name: "beatmania IIDX",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "Big Buck Reloaded",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting"
+  },
+  {
+    name: "Big Ticket Drop",
+    manufacturer: "BayTek",
+    primary: "Redemption"
+  },
+  {
+    name: "Boardwalk Big Spin Jr",
+    manufacturer: "Coastal",
+    primary: "Redemption"
+  },
+  {
+    name: "Bop It!",
+    manufacturer: "SEGA",
+    primary: "Redemption"
+  },
+  {
+    name: "Break the Plate",
+    manufacturer: "Coastal",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Bust-a-Move Frenzy",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting"
+  },
+  {
+    name: "Carnival Wheel",
+    manufacturer: "Teamplay",
+    primary: "Redemption"
+  },
+  {
+    name: "Castlevania: Marble of Souls",
+    manufacturer: "Konami",
+    primary: "Redemption"
+  },
+  {
+    name: "Centipede Chaos",
+    manufacturer: "ICE",
+    primary: "Redemption"
+  },
+  {
+    name: "Chibi Crane",
+    manufacturer: "KNT",
+    primary: "Prize"
+  },
+  {
+    name: "Chrono Circle",
+    manufacturer: "Andamiro",
+    primary: "Music"
+  },
+  {
+    name: "Chinithm Paradise Lost",
+    manufacturer: "SEGA",
+    primary: "Music"
+  },
+  {
+    name: "Connect 4 Deluxe",
+    manufacturer: "Baytek",
+    primary: "Redemption"
+  },
+  {
+    name: "Connect 4 Hoops",
+    manufacturer: "Baytek",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Cosmic Tower",
+    manufacturer: "LAI",
+    primary: "Redemption"
+  },
+  {
+    name: "Crossy Road",
+    manufacturer: "Adrenaline",
+    primary: "Redemption"
+  },
+  {
+    name: "DANCE aROUND",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "Dancerush Stardom",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "Dark Escape 4D",
+    manufacturer: "Namco",
+    primary: "Shooting"
+  },
+  {
+    name: "Daytona USA 3 DX UD Shift",
+    manufacturer: "Play It? / SEGA",
+    primary: "Racing"
+  },
+  {
+    name: "DC Comics 4P Coin",
+    manufacturer: "Andamiro",
+    primary: "Redemption"
+  },
+  {
+    name: "DDR Ace Gold",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "DDR Ace White",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "Deal or No Deal",
+    manufacturer: "ICE",
+    primary: "Redemption"
+  },
+  {
+    name: "Deca Cre",
+    manufacturer: "KNT",
+    primary: "Prize"
+  },
+  {
+    name: "Dizzy Chicken",
+    manufacturer: "BayTek",
+    primary: "Redemption"
+  },
+  {
+    name: "Down the Clown",
+    manufacturer: "ICE",
+    primary: "Redemption"
+  },
+  {
+    name: "Drop it Drop it",
+    manufacturer: "Komuse",
+    primary: "Redemption"
+  },
+  {
+    name: "Extreme Air FX",
+    manufacturer: "SAM-e",
+    primary: "Sports"
+  },
+  {
+    name: "F&F Super Cars",
+    manufacturer: "Raw Thrills",
+    primary: "Racing"
+  },
+  {
+    name: "F&F Turbo",
+    manufacturer: "Raw Thrills",
+    primary: "Racing"
+  },
+  {
+    name: "Fantastic Prize",
+    manufacturer: "UNIS",
+    primary: "Prize"
+  },
+  {
+    name: "Fantasy Soccer",
+    manufacturer: "UNIS",
+    primary: "Sports"
+  },
+  {
+    name: "Full Tilt",
+    manufacturer: "BayTek",
+    primary: "Redemption"
+  },
+  {
+    name: "Fun Zone",
+    manufacturer: "CTC",
+    primary: "Prize"
+  },
+  {
+    name: "Fusion Skeeball",
+    manufacturer: "Baytek",
+    primary: "Redemption"
+  },
+  {
+    name: "Galaxy Hunter",
+    manufacturer: "LAI",
+    primary: "Redemption"
+  },
+  {
+    name: "Gemini Big",
+    manufacturer: "KNT",
+    primary: "Prize"
+  },
+  {
+    name: "Gemini Mini",
+    manufacturer: "KNT",
+    primary: "Prize"
+  },
+  {
+    name: "Ghostbusters Arcade",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting"
+  },
+  {
+    name: "GITADORA DrumMania XG DX",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "GITADORA DrumMania ARENA",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "GITADORA GuitarFreaks XG DX",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "GITADORA GuitarFreaks ARENA",
+    manufacturer: "Konami",
+    primary: "Music"
+  },
+  {
+    name: "Goal Line Rush",
+    manufacturer: "Namco",
+    primary: "Sports"
+  },
+  {
+    name: "Goatz & Ropes",
+    manufacturer: "Baytek",
+    primary: "Redemption"
+  },
+  {
+    name: "Godzilla Kaiju Wars",
+    manufacturer: "Raw Thrills",
+    primary: "VR | Shooting"
+  },
+  {
+    name: "Gold Railroad",
+    manufacturer: "Konami",
+    primary: "Redemption"
+  },
+  {
+    name: "Grand Piano Keys",
+    manufacturer: "BayTek",
+    primary: "Redemption"
+  },
+  {
+    name: "GROOVE COASTER",
+    manufacturer: "SEGA",
+    primary: "Music"
+  },
+  {
+    name: "Halo: Fireteam Raven 4P",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting"
+  },
+  {
+    name: "Hippare-Q",
+    manufacturer: "Taito",
+    primary: "Sports"
+  },
+  {
+    name: "Hot Shot",
+    manufacturer: "BayTek",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "House of the Dead Scarlet Dawn",
+    manufacturer: "UNIS",
+    primary: "Shooting"
+  },
+  {
+    name: "Hungry Hungry Hippos",
+    manufacturer: "Adrenaline",
+    primary: "Redemption"
+  },
+  {
+    name: "HYPERpitch",
+    manufacturer: "LAI",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "HYPERshoot",
+    manufacturer: "LAI",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Ice Man",
+    manufacturer: "Coastal",
+    primary: "Shooting",
+    secondary: "Redemption"
+  },
+  {
+    name: "IceBall FX",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Initial D8",
+    manufacturer: "SEGA",
+    primary: "Racing"
+  },
+  {
+    name: "Injustice Arcade 43\"",
+    manufacturer: "Raw Thrills",
+    primary: "Fighting",
+    secondary: "Redemption"
+  },
+  {
+    name: "Intercard POS (& Redemption)",
+    manufacturer: "Intercard",
+    primary: "Other"
+  },
+  {
+    name: "Jumbo Jumpin'",
+    manufacturer: "Namco",
+    primary: "Sports",
+    secondary: ""
+  },
+  {
+    name: "Jurassic Park Arcade",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting"
+  },
+  {
+    name: "King Kong of Skull Island",
+    manufacturer: "Raw Thrills",
+    primary: "VR"
+  },
+  {
+    name: "Lane Master",
+    manufacturer: "UNIS",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Let's Go Island: Dream Edition",
+    manufacturer: "SEGA",
+    primary: "Shooting"
+  },
+  {
+    name: "Luigi's Mansion",
+    manufacturer: "SEGA",
+    primary: "Shooting"
+  },
+  {
+    name: "Magician's Wheel",
+    manufacturer: "SEGA",
+    primary: "Redemption"
+  },
+  {
+    name: "Mario Kart Arcade GP DX",
+    manufacturer: "Namco",
+    primary: "Racing"
+  },
+  {
+    name: "Marvel Contest of Champions",
+    manufacturer: "Raw Thrills",
+    primary: "Fighting",
+    secondary: "Redemption"
+  },
+  {
+    name: "Marvel's Avenger 2P",
+    manufacturer: "Andamiro",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "Maximum Tune 5DX+",
+    manufacturer: "Namco",
+    primary: "Racing",
+    secondary: "N/A"
+  },
+  {
+    name: "MT5DX+ Wangan Terminal",
+    manufacturer: "Namco",
+    primary: "Racing",
+    secondary: "Other"
+  },
+  {
+    name: "Mega Blaster",
+    manufacturer: "touchmagix",
+    primary: "Redemption"
+  },
+  {
+    name: "Mega Stacker",
+    manufacturer: "LAI",
+    primary: "Redemption"
+  },
+  {
+    name: "Milk Jug Toss",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Minecraft Dungeons Arcade",
+    manufacturer: "Raw Thrills",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Mission Impossible Arcade",
+    manufacturer: "SEGA",
+    primary: "Shooting",
+    secondary: "N/A"
+  },
+  {
+    name: "Monopoly Roll-N-Go",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Monster Drop X-treme",
+    manufacturer: "Benchmark",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "MUSECA",
+    manufacturer: "Konami",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Music Diver",
+    manufacturer: "Taito",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Naruto: Emblem Battle",
+    manufacturer: "Marvelous",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "NBA Gametime",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "NBA Hoops",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "NBA Superstars",
+    manufacturer: "Raw Thrills",
+    primary: "Sports",
+    secondary: "N/A"
+  },
+  {
+    name: "NERF",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting",
+    secondary: "Redemption"
+  },
+  {
+    name: "NESiCAxLive (Vewlix)",
+    manufacturer: "Taito",
+    primary: "Various",
+    secondary: "Other"
+  },
+  {
+    name: "Nitro Trucks",
+    manufacturer: "Raw Thrills",
+    primary: "Racing",
+    secondary: "N/A"
+  },
+  {
+    name: "OnPoint",
+    manufacturer: "UNIS",
+    primary: "Shooting",
+    secondary: "Redemption"
+  },
+  {
+    name: "Over The Edge",
+    manufacturer: "UNIS",
+    primary: "Prize",
+    secondary: "N/A"
+  },
+  {
+    name: "Pearl Fishery",
+    manufacturer: "LAI",
+    primary: "Redemption"
+  },
+  {
+    name: "Deadpool",
+    manufacturer: "Stern",
+    primary: "Pinball",
+    secondary: "N/A"
+  },
+  {
+    name: "GoTG",
+    manufacturer: "Stern",
+    primary: "Pinball",
+    secondary: "N/A"
+  },
+  {
+    name: "Star Trek",
+    manufacturer: "Stern",
+    primary: "Pinball",
+    secondary: "N/A"
+  },
+  {
+    name: "Pirates Hook 4P",
+    manufacturer: "UNIS",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Pixel Chase",
+    manufacturer: "SEGA",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Plinko",
+    manufacturer: "Coastal",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Pong",
+    manufacturer: "UNIS",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Pop It",
+    manufacturer: "touchmagix",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Power Drop X-Treme",
+    manufacturer: "Benchmark",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Power Roll",
+    manufacturer: "SEGA",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Prize Cube 6P",
+    manufacturer: "Coast to Coast",
+    primary: "Prize",
+    secondary: "N/A"
+  },
+  {
+    name: "Project Diva",
+    manufacturer: "SEGA",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Pump it Up Phoenix",
+    manufacturer: "Andamiro",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Quad Air Hockey",
+    manufacturer: "Barron",
+    primary: "Sports",
+    secondary: "N/A"
+  },
+  {
+    name: "Quarterback Pro",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Rick & Morty: Chips & Blitz",
+    manufacturer: "LAI",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "Street Fighter VI (Vewlix)",
+    manufacturer: "Taito / Capcom",
+    primary: "Fighting",
+    secondary: "N/A"
+  },
+  {
+    name: "Scooby-Doo",
+    manufacturer: "BayTek",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Sink It",
+    manufacturer: "BayTek",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Skill Fall Revolution",
+    manufacturer: "UNIS",
+    primary: "Prize",
+    secondary: "N/A"
+  },
+  {
+    name: "Smash Stadium",
+    manufacturer: "Konami",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "Snowboarder (X-Games)",
+    manufacturer: "Raw Thrills",
+    primary: "Sports",
+    secondary: "N/A"
+  },
+  {
+    name: "Sonic Air Hockey",
+    manufacturer: "SEGA",
+    primary: "Sports",
+    secondary: "N/A"
+  },
+  {
+    name: "Sonic Basketball 2019",
+    manufacturer: "SEGA",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Sound Voltex: Valkyrie",
+    manufacturer: "Konami",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Space Warp66",
+    manufacturer: "touchmagix",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Speed of Light",
+    manufacturer: "LAI",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Spider Boxer",
+    manufacturer: "Kalkomat",
+    primary: "Sports",
+    secondary: "N/A"
+  },
+  {
+    name: "Spider-Man 2P",
+    manufacturer: "Andamiro",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "SpongeBob Pirates of Bikini Bottom 2P",
+    manufacturer: "Andamiro",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "Star Wars 2P",
+    manufacturer: "Andamiro",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    name: "StepManiaX",
+    manufacturer: "Step Revolution",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Super Bikes 3",
+    manufacturer: "Raw Thrills",
+    primary: "Racing",
+    secondary: "N/A"
+  },
+  {
+    name: "Sync Pong",
+    manufacturer: "SEGA",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Taiko no Tatsujin (2024)",
+    manufacturer: "Taito",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Tetote x Connect",
+    manufacturer: "Taito",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "The Walking Dead",
+    manufacturer: "Raw Thrills",
+    primary: "Shooting",
+    secondary: "N/A"
+  },
+  {
+    name: "Tic Shaq Toe",
+    manufacturer: "Andamiro",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    name: "Ticket Dome",
+    manufacturer: "UNIS",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Ticket Man",
+    manufacturer: "Andamiro",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Ticket Monster",
+    manufacturer: "BayTek",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Toilet Bowl Toss",
+    manufacturer: "BayTek",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Tomb Raider",
+    manufacturer: "Adrenaline",
+    primary: "Shooting",
+    secondary: "N/A"
+  },
+  {
+    name: "Tons of Tickets",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Tower of Tickets",
+    manufacturer: "BayTek",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Treasure Quest",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "TRYPOD",
+    manufacturer: "Marvelous",
+    primary: "Prize",
+    secondary: "N/A"
+  },
+  {
+    name: "UFO 8 Catcher",
+    manufacturer: "SEGA",
+    primary: "Prize",
+    secondary: "N/A"
+  },
+  {
+    name: "VR Agent",
+    manufacturer: "UNIS",
+    primary: "Shooting",
+    secondary: "VR"
+  },
+  {
+    name: "WACCA",
+    manufacturer: "Andamiro",
+    primary: "Music",
+    secondary: "N/A"
+  },
+  {
+    name: "Waterfuls",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Whack 'N Win",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    name: "Whack-a-Clown",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    description: "Coin pusher with Willy Wonka-themed character cards as (ticket) redemption collectibles",
+    name: "Willy Wonka 2P",
+    manufacturer: "ELAUT",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    description: "Spin the wheel (magical ball) game w/ ticket value(s) or Wizard of Oz-themed character card rewards corresponding to final landing position",
+    name: "Wizard of Oz",
+    manufacturer: "Raw Thrills",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    description: "Coin pusher with Wizard of Oz-themed character cards as (ticket) redemption collectibles",
+    name: "Wizard of Oz 6P",
+    manufacturer: "ELAUT",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    description: "Skill-based football game w/ targets corresponding to ticketed payout value(s)",
+    name: "World Football Pro",
+    manufacturer: "ICE",
+    primary: "Sports",
+    secondary: "Redemption"
+  },
+  {
+    description: "Enhanced (2P) version of original Pac-Man game on a 108\" LED display",
+    name: "World's Largest Pac-Man",
+    manufacturer: "Raw Thrills",
+    primary: "N/A"
+  },
+  {
+    description: "Coin pusher with WWE-themed character cards as (ticket) redemption collectibles",
+    name: "WWE Superstar Rumble 2P",
+    manufacturer: "Andamiro",
+    primary: "Coin Pusher",
+    secondary: "Redemption"
+  },
+  {
+    description: "Skill-based (4P) ball game w/ targets corresponding to running speed/distance determining the game's race winner",
+    name: "Zombies Ready Deady Go",
+    manufacturer: "Play It / SEGA",
+    primary: "Redemption",
+    secondary: "N/A"
+  },
+  {
+    description: "Skill-based game (6P) w/ swinging arms used to move RFID ticket pucks into win chutes",
+    name: "Zombie Snatcher",
+    manufacturer: "ICE",
+    primary: "Redemption",
+    secondary: "N/A"
+  }
+]
